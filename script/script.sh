c50f9c32fd843aa9744fb85bb8ed2cf5b7a2ab32
mael-pignol <31483567+mael-pignol@users.noreply.github.com>
2017-08-31 11:01:12 +0200

    Add files via upload

---
+++ script/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+A=$(sed -n 1p coordonnees.txt)
+B=$(sed -n 2p coordonnees.txt)
+C=$(sed -n 3p coordonnees.txt)
+D=$(sed -n 4p coordonnees.txt)
+E=$(sed -n 5p coordonnees.txt)
+F=$(sed -n 6p coordonnees.txt)
+G=$(sed -n 7p coordonnees.txt)
+H=$(sed -n 8p coordonnees.txt)
+I=$(sed -n 9p coordonnees.txt)
+
+CHEMIN='/home/prof/troispartrois/'
+
+IP='172.26.69.12'
+USER='pignol'
+
+ssh $USER@$IP cat $CHEMIN/$A $CHEMIN/$D $CHEMIN/$G > 1.fodaly
+ssh $USER@$IP cat $CHEMIN/$B $CHEMIN/$E $CHEMIN/$H > 2.fodaly
+ssh $USER@$IP cat $CHEMIN/$C $CHEMIN/$F $CHEMIN/$I > 3.fodaly
+
+cat 1.fodaly espace.fodaly 2.fodaly espace.fodaly 3.fodaly > lab.fodaly
+
+cd ..
+cd src
+
+echo "DEBUT DE RESOLUTION : ALGORYTHME RANDOM"
+gcc -std=c99 random.c -L. -llabyrinthes -lcairo -lX11
+./a.out
---
+++ script/scriptnouveau.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+A=$(sed -n 1p ptdr.txt)
+B=$(sed -n 2p ptdr.txt)
+C=$(sed -n 3p ptdr.txt)
+D=$(sed -n 4p ptdr.txt)
+E=$(sed -n 5p ptdr.txt)
+F=$(sed -n 6p ptdr.txt)
+G=$(sed -n 7p ptdr.txt)
+H=$(sed -n 8p ptdr.txt)
+I=$(sed -n 9p ptdr.txt)
+
+CHEMIN='/home/prof/troispartrois/'
+
+IP='172.26.69.12'
+USER='pignol'
+
+ssh $USER@$IP cat $CHEMIN/$A $CHEMIN/$D $CHEMIN/$G > 1.fodaly
+ssh $USER@$IP cat $CHEMIN/$B $CHEMIN/$E $CHEMIN/$H > 2.fodaly
+ssh $USER@$IP cat $CHEMIN/$C $CHEMIN/$F $CHEMIN/$I > 3.fodaly
+
+cat 1.fodaly espace.fodaly 2.fodaly espace.fodaly 3.fodaly > lab.fodaly
+
+cd ..
+cd src
+
+echo "DEBUT DE RESOLUTION : ALGORYTHME PLEDGE"
+gcc -std=c99 pledge.c -L. -llabyrinthes -lcairo -lX11
+./a.out
